7608ae516e1f46124314ce1031db10d0a0dbf765
pavithranath <pavithranath@cloudrevelinnovation.com>
2025-04-16 17:03:34 +0530

    categories image upload succesfully

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,16 +1,17 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "./cloudinary.js";
+import path  from "path";
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: (req, file) => {
     const folderType = req.folderType || "misc";
-    // console.log("folderType",folderType)
+    console.log("file",file)
     return {
       folder: folderType === "category" ? "category_image" : "product_image",
-      // allowed_formats: ["jpg", "png", "jpeg", "webp", "svg"],
-      public_id: `${Date.now()}-${file.originalname}`,
+      allowed_formats: ["jpg", "png", "jpeg", "webp"],
+      public_id: `${Date.now()}-${path.parse(file.originalname).name}`,
       resource_type: "auto",
     };
   },
